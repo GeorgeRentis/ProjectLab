63aabb71abaf29fba9be96e4386b86262c06184c
George Rentis <georent123@hotmail.com>
2015-11-06 19:16:46 +0200

    trying to fix forgot password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ config.action_mailer.raise_delivery_errors = true
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
+  
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
