e1e708ced3b4557921947b842c358aeb7b5f8bf9
George Rentis <georent123@hotmail.com>
2015-12-07 03:07:05 +0200

    fixed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,10 @@
 class CategoriesController < ApplicationController
   def index
-    @categories = Category.all
+  	if current_user
+    	@categories = Category.all
+    else
+      flash[:danger] = "You have to register to access this area"
+      redirect_to root_path
+    end
   end
 end
