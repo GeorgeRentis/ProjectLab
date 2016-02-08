d7f6a80756d0293ae2f9a2e09cdb0deee78dd2ad
GeorgeRentis <georent123@hotmail.com>
2016-02-08 04:07:56 +0200

    fixed stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :images, as: :uploadable, dependent: :destroy
   has_many :comments,dependent: :destroy
   has_many :lessons,dependent: :destroy
-  has_many :lesson_ratings
+  has_many :lesson_ratings, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   devise :database_authenticatable,:recoverable, :rememberable, :trackable, :validatable
   enum role_id: [ :admin, :user ]
