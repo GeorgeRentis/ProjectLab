d6ed914916aa000325a1710cc3f5f548088e0b46
GeorgeRentis <georent123@hotmail.com>
2016-02-09 14:55:51 +0200

    fixed stuff

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,10 @@
 class CategoriesController < ApplicationController
   def index
-  	if current_user
-    	@categories = Category.all
-    else
-      flash[:danger] = "You have to register to access this area"
-      redirect_to root_path
-    end
+  	# if current_user
+     	@categories = Category.all
+   #  else
+   #    flash[:danger] = "You have to register to access this area"
+   #    redirect_to root_path
+   #  end
   end
 end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,15 +1,13 @@
 class LessonsController < ApplicationController
   def index
-    if current_user
       if params[:category_id]!=nil
         @lessons = Lesson.where(category_id: params[:category_id])
       else
         @lessons = Lesson.search(params[:search])
       end
-    else
-      flash[:danger] = "You have to register to access this area"
-      redirect_to root_path
-    end
+    #   flash[:danger] = "You have to register to access this area"
+    #   redirect_to root_path
+    # end
   end
 
   def show
