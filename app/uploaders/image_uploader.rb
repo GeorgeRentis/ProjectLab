1b9f84b24c51ca7d27e691c500bc6324ac09751e
GeorgeRentis <georent123@hotmail.com>
2017-02-20 13:48:10 -0500

    commit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,14 +4,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   include Sprockets::Rails::Helper
   include CarrierWave::RMagick
-  include CarrierWave::MimeTypes
+  
   process :set_content_type
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+
   # storage :fog
   version :thumb do
     process resize_to_fit: [50, 50]
